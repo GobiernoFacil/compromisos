8d11a83a37af762db77798ee088557b069c9dc97
hugovom <hugo.om@gmail.com>
2014-07-23 15:24:47 -0500

    agrega semantica html

---
+++ public/html_slicing/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Tablero de control público de seguimiento del Plan de Acción 2013-2015 México</title>
+		<link rel="shortcut icon" href="http://assets.pa2015.mx/frontend/img/logo.png" />
+		<!--[if lt IE 9]>
+		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300|Roboto:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="/css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="/css/styles.css" />
+		<meta name="viewport" content="initial-scale=1.0" />
+	</head>
+	
+<body><header id="header">
+	<div class="container">
+		<h1><a href="#header" class="slideto">Alianza para el Gobierno Abierto - Plan de Acción 2013-2015 México</a></h1>
+		<nav>
+			<ul>
+				<li><a href="#" >Misión y Objetivos</a></li>
+				<li><a href="#plan">Blog</a></li>
+				<li><a href="#plan">Antecedentes</a></li>
+				<li><a href="#plan">Preguntas Frecuentes</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
+
+<section id="alianza">
+	<div class="container">
+
+			
+	</div>
+<footer id="footer">
+	<div class="container">
+		
+		<ul class="logos">
+			<li><span><span><a href="http://www.presidencia.gob.mx/" class="logo logo-gobierno" target="_blank">México - Gobierno de la República</a></span></span></li>
+			<li><span><span><a href="http://www.presidencia.gob.mx/edn/" class="logo logo-mexico-abierto" target="_blank">México Abierto</a></span></span></li>
+			<li><span><span><a href="http://inicio.ifai.org.mx/_catalogs/masterpage/ifai.aspx" class="logo logo-ifai" target="_blank">IFAI</a></span></span></li>
+			<li><span><span><a href="http://www.culturaecologica.org.mx/ " class="logo logo-cultura-ecologica" target="_blank">Cultura Ecológica</a></span></span></li>
+			<li><span><span><a href="http://fundar.org.mx/index.html/ " class="logo logo-fundar" target="_blank">Fundar</a></span></span></li>
+			<li><span><span><a href="http://imco.org.mx/home/" class="logo logo-imco" target="_blank">IMCO</a></span></span></li>
+			<li><span><span><a href="http://articulo19.org/" class="logo logo-article19" target="_blank">Article19</a></span></span></li>
+			<li><span><span><a href="http://www.cidac.org/esp/index.php" class="logo logo-cidac" target="_blank">CIDAC</a></span></span></li>
+			<li><span><span><a href="http://www.gesoc.org.mx/site/" class="logo logo-gesoc" target="_blank">gesoc</a></span></span></li>
+			<li><span><span><a href="http://www.tm.org.mx/c/inicio/" class="logo logo-transparencia-mexicana" target="_blank">Transparencia Mexicana</a></span></span>
+			</li>
+			<li><span><span><a href="http://socialtic.org/" class="logo logo-social-tic" target="_blank">Social Tic</a></span></span></li>
+		</ul>
+		
+	</div>
+</footer>	
+</body>
+</html>
