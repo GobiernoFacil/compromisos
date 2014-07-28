692fca3759c35a015487aeec929eb707ef3609e9
hugovom <hugo.om@gmail.com>
2014-07-28 00:19:11 -0500

    test de view

--- app/routes.php
+++ app/routes.php
@@ -6,11 +6,12 @@
 |--------------------------------------------------------------------------
 |
 | Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
+| Its a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the Closure to execute when that URI is requested.
 |
 */
 
+Route::get('/', function(){return View::make('home');});
 Route::controller('/compromisos','CompromisosFrontendController');
 Route::controller('/backend/auth','AuthController');
 
---
+++ app/views/home.blade.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>hola</title>
+</head>
+<body>
+	<p>Hola amigos</p>
+	{{ var_dump($_ENV)}}
+</body>
+</html>
