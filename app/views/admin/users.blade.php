e2fc9f143894082fedb9bf951b9116c77148ee83
el coruco <el.coruco@gmail.com>
2014-08-09 14:39:15 -0500

    la lista de usuarios y agregar mi compu a la lista de localhosteses. localhosteses.

--- app/views/admin/users.blade.php
+++ app/views/admin/users.blade.php
@@ -1,8 +1,50 @@
 @extends('backend')
+<!-- users menu -->
+<ul>
+  <li>{{link_to('admin/usuarios/agregar', 'agregar usuario')}}</li>
+</ul>
 
-<!-- users list -->
+<!-- users table -->
 @section('content')
-  <pre>
-    {{count($users)}}
-  </pre>
+  <table>
+    <thead>
+      <tr>
+        <th>user/email</th>
+        <th>nombre</th>
+        <th>teléfono</th>
+        <th>cargo</th>
+        <th>es de gobierno</th>
+        <th>es administrador</th>
+        <th>*</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($users as $user)
+      <tr>
+        <td>{{$user->username}}</td>
+        <td>{{$user->name}}</td>
+        <td>{{$user->phone}}</td>
+        <td>{{$user->charge}}</td>
+        <td>{{$user->user_type == 'government' ? 'Sí':'No'}}</td>
+        <td>{{$user->is_admin ? 'Sí':'No'}}</td>
+        <td>
+          <!-- update link -->
+          {{link_to('admin/usuario/' . $user->id, 'editar')}}
+
+          <!-- delete form -->
+          @if($user->id != Auth::user()->id)
+            {{Form::open([
+              'url' => 'admin/usuario/' . $user->id, 
+              'method' => 'delete'
+            ])}}
+
+            <input type="submit" value="eliminar">
+
+            {{Form::close()}}
+          @endif
+        </td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
 @stop
