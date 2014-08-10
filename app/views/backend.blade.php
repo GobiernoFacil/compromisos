c23adbaae537cc855255e5b8d72034295eac6959
hugovom <hugo.om@gmail.com>
2014-08-09 19:14:36 -0500

    agrega bootstrap para el admin

--- app/views/backend.blade.php
+++ app/views/backend.blade.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Backend</title>
+	<meta name="description" content="Backend">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="/css/admin/bootstrap.min.css">
+	<link rel="stylesheet" href="/css/admin/docs.min.css">
 </head>
 <body>
   @yield('content')
