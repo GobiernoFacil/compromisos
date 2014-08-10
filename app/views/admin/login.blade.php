a74553d6bb7d9ceb90719fdf3258a932bb8ed39d
hugovom <hugo.om@gmail.com>
2014-08-09 19:59:19 -0500

    agrega semantica y nav

--- app/views/admin/login.blade.php
+++ app/views/admin/login.blade.php
@@ -1,16 +1,26 @@
-@extends('frontend')
+@extends('backend')
 
 <!-- THE LOGIN FORM -->
 @section('content')
-  {{Form::open()}}
+<div class="container">
+ <div class="row">
+ 	 <div class="col-sm-6 col-md-4 col-md-offset-4">
+ 	 	<h1 class="login-title">Ingresa al Tablero de Compromisos</h1>
+
+  {{Form::open([
+  	"class"=> "form-signin"
+  ])}}
   <p>
     <label for="username">correo:</label>
-    <input type="text" name="username" id="username">
+    <input type="text" class="form-control" name="username" id="username">
   </p>
   <p>
     <label for="password">contraseña:</label>
-    <input type="password" name="password" id="password">
+    <input type="password" class="form-control" name="password" id="password">
   </p>
-  <p><input type="submit" value="Acceder"></p>
+  <p><input type="submit" class="btn btn-lg btn-primary btn-block" value="Acceder"></p>
   {{Form::close()}}
+ 	 </div>
+ </div>
+</div>
 @stop
