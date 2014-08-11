40f439eb92ed83fc3da1c5d2054f0be8f1b067e8
hugovom <hugo.om@gmail.com>
2014-08-11 11:50:37 -0500

    modifica html

--- app/views/admin/commitments_add.blade.php
+++ app/views/admin/commitments_add.blade.php
@@ -1,27 +1,46 @@
 @extends('backend')
 
 @section('content')
-  {{Form::open(['url' => 'commitment'])}}
-    <p>
-      <label for="title">Título: </label>
-      <input type="text" name="title" id="title">
-    </p>
-    <p>
-      <label for="plan">Plan: </label>
-      <input type="text" name="plan" id="plan">
-    </p>
-
-    <p>
-    </p>
-      <label for="government_user">Usuario de gobierno</label>
-      {{Form::select('government_user', $government_users)}}
-    <p>
-    </p>
-      <label for="society_user">Usuario externo</label>
-      {{Form::select('society_user', $society_users)}}
-    <p>
-  
-
-    <p><input type="submit" value="guardar"></p>
+@include('backend_nav')
+<div class="container">
+	<h1>Crear compromiso</h1>
+  {{Form::open(['url' => 'commitment',
+  'class'=>'form-horizontal'
+  ])}}
+  <!--título-->
+  <div class="form-group">
+      <label for="title" class="col-sm-2 control-label">Título: </label>
+	  <div class="col-sm-8">
+      	<input type="text" name="title"  class="form-control" id="title">
+	  </div>
+  </div>
+  <!--Plan-->
+  <div class="form-group">
+      <label for="plan" class="col-sm-2 control-label">Plan: </label>
+	  <div class="col-sm-8">
+      	<input type="text" name="plan"  class="form-control" id="plan">
+	  </div>
+  </div>
+  <!--Usuario de Gobierno-->
+  <div class="form-group">
+      <label for="government_user" class="col-sm-2 control-label">Usuario de gobierno</label>
+	  <div class="col-sm-8">
+      {{Form::select('government_user', $government_users, FALSE, ['class'=>'form-control'])}}
+	  </div>
+   </div>
+   <!--Usuario externo-->
+  <div class="form-group">
+      <label for="society_user" class="col-sm-2 control-label">Usuario externo</label>
+	  <div class="col-sm-8">
+      {{Form::select('society_user', $society_users, FALSE, ['class'=>'form-control'])}}
+	  </div>
+   </div>
+   <!--submit-->
+  <div class="form-group">
+  	<div class="col-sm-8 col-sm-offset-2">
+    <input type="submit" class="btn btn-lg btn-primary" value="Guardar">
+  	</div>
+  </div>
   {{Form::close()}}
+</div>
 @stop
