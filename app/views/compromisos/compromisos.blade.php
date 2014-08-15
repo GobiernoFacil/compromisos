fed6cfa16b436b250c5e4a24ca4450c230e16094
hugovom <hugo.om@gmail.com>
2014-08-15 05:17:12 -0500

    actualiza estructura y estilo en frontend

--- app/views/compromisos/compromisos.blade.php
+++ app/views/compromisos/compromisos.blade.php
@@ -4,38 +4,39 @@
 <section id="tablero">
 	<div class="container">
 		<div class="row list-title">
-			<div class="col-md-1">
+			<div class="col-sm-1">
 			</div>
-			<div class="col-md-3">
+			<div class="col-sm-3">
 				<h3>Compromisos</h3>
 			</div>
-			<div class="col-md-2">
+			<div class="col-sm-2">
 				<h3>PRIMER AVANCE <small>27 de octubre de 2014</small></h3>
 			</div>
-			<div class="col-md-2">
+			<div class="col-sm-2">
 				<h3>SEGUNDO AVANCE 	<small>8 de marzo de 2015</small></h3>
 			</div>
-			<div class="col-md-2">
+			<div class="col-sm-2">
 				<h3>TERCER AVANCE <small>22 de julio de 2015</small></h3>
 			</div>
-			<div class="col-md-2">
+			<div class="col-sm-2">
 				<h3>RESULTADO FINAL</h3>
 			</div>
 		</div><!-- ends list-title-->
 		
 		<!-- comienza lista-->
 		<div class="row list-key">
-			<div class="col-md-1 id">
+			<div class="col-xs-3 col-sm-1 id">
 				<h4>1</h4>
 				<div class="plus">
 					<a href="#" title="responsable-1">+</a>
 				</div>
 			</div>
-			<div class="col-md-3 ct">
+			<div class="col-xs-9 col-sm-3 ct">
 				<p>Tu gobierno en un solo punto | gob.mx</p>
 			</div>
-			<!-- primer avance-->
-			<div class="col-md-2 ct">
+			<section class="mobile">
+				<!-- primer avance-->
+				<div class="col-xs-12  col-sm-2 ct">
 				<!-- lista de objetivos y status-->
 				<ul class="cumplimiento">
 					<li><a href="#" class="objetivo completado"></a>
@@ -83,8 +84,8 @@
 						
 				</ul>
 			</div>
-			<!-- segundo avance-->
-			<div class="col-md-2 ct">
+				<!-- segundo avance-->
+				<div class="col-xs-12  col-sm-2 ct">
 				<!-- lista de objetivos y status-->
 				<ul class="cumplimiento">
 					<li><a href="#" class="objetivo proceso"></a>
@@ -132,8 +133,8 @@
 								
 				</ul>			
 			</div>
-			<!-- terce avance-->
-			<div class="col-md-2 ct">
+				<!-- terce avance-->
+				<div class="col-xs-12  col-sm-2 ct">
 				<!-- lista de objetivos y status-->
 				<ul class="cumplimiento">
 					<li><a href="#" class="objetivo sin_avance"></a>
@@ -180,8 +181,8 @@
 					</li>					
 				</ul>
 			</div>
-			<!-- resultado-->
-			<div class="col-md-2 ct">
+				<!-- resultado-->
+				<div class="col-xs-12  col-sm-2 ct">
 				<ul>
 					<li class="resultado_link">	<a>www.origen.org</a>
 					<ul class="resultado">
@@ -194,16 +195,17 @@
 					</li>
 				</ul>
 			</div>
+			</section>
 		</div><!-- ends list-->
 		<!-- info responsable-->
 		<div id="responsable-1" class="row list-responsable">
-			<div class="col-md-4 col-md-offset-1 ct">
+			<div class="col-sm-4 col-sm-offset-1 ct">
 				<p>Plataforma virtual la información sobre las Normas Oficiales Mexicanas Vigentes.
 					<a href="#">Consulta el plan de trabajo</a>
 				</p> 
 				
 			</div>
-			<div class="col-md-3 col-md-offset-1 ct">
+			<div class="col-sm-3 col-sm-offset-1 ct">
 				<h5>Responsable</h5>
 				<p class="vcard">
 					<span class="fn">Alberto Ulises Esteban Marina</span>
@@ -212,7 +214,7 @@
 					<a href="mailto:aulisesmarina@economia.gob.mx">aulisesmarina@economia.gob.mx</a>
 				</p>
 			</div>
-			<div class="col-md-3 ct">
+			<div class="col-sm-3 ct">
 				<h5>Responsable de la Organización de la Sociedad Civil</h5>
 				<p class="vcard">
 					<span class="fn">Ricardo Corona Real</span>
