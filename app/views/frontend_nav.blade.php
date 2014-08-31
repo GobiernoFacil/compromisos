ce00eea088af73089de298982085756d962c8164
hugovom <hugo.om@gmail.com>
2014-08-30 21:31:10 -0500

    actualizando enlaces en header del front

--- app/views/frontend_nav.blade.php
+++ app/views/frontend_nav.blade.php
@@ -4,12 +4,13 @@
 			<div class="col-md-1">
 				<h1><a href="/" class="slideto">Alianza para el Gobierno Abierto - Plan de Acción 2013-2015 México</a></h1>
 			</div>
-			<nav class="col-md-5 col-md-offset-6">
+			<nav class="col-md-6 col-md-offset-5">
 				<ul>
-				<li><a href="#" >Misión y Objetivos</a></li>
-				<li><a href="#plan">Blog</a></li>
+				<li><a href="http://aga.ensambledev.com/mision-y-objetivos/">Misión y Objetivos</a></li>
 				<li><a href="#plan">Antecedentes</a></li>
-				<li><a href="#plan">Preguntas Frecuentes</a></li>
+				<li><a href="http://aga.ensambledev.com/blog/">Blog</a></li>
+				<li><a href="http://aga.ensambledev.com/preguntas-frecuentes/">Preguntas Frecuentes</a></li>
+				<li><a href="http://tablero.inegifacil.com/">Tablero</a></li>
 				</ul>
 			</nav>
 		</div>
