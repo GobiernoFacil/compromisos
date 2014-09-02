a23b9a041b39dc1889c30b31a9f2faf35dc7fb0b
hugovom <hugo.om@gmail.com>
2014-09-02 01:45:52 -0500

    actualiza diseño del login, se veía muy plano

--- app/views/admin/login.blade.php
+++ app/views/admin/login.blade.php
@@ -1,26 +1,38 @@
-@extends('backend', ['title' => 'Login | Tablero de control público de seguimiento del PA15.'])
+@extends('frontend', ['title' => 'Login | Tablero de control público de seguimiento del PA15.'])
 
 <!-- THE LOGIN FORM -->
 @section('content')
+@include('frontend_nav')
+<main role="main" class="main">
+<section id="alianza">
 <div class="container">
- <div class="row">
- 	 <div class="col-sm-6 col-md-4 col-md-offset-4">
- 	 	<h1 class="login-title">Ingresa al Tablero de Compromisos</h1>
-
-  {{Form::open([
-  	"class"=> "form-signin"
-  ])}}
-  <p>
-    <label for="username">correo:</label>
-    <input type="text" class="form-control" name="username" id="username">
-  </p>
-  <p>
-    <label for="password">contraseña:</label>
-    <input type="password" class="form-control" name="password" id="password">
-  </p>
+	<div class="row">
+		<header class="main-header">
+			<h1 class="first-word-bold"><strong>Ingresa</strong> al Tablero de Compromisos</h1>
+		</header>
+		<section class="section-page">
+			<div class="col-sm-6 col-sm-offset-3">
+			<article>
+				<div class="post-entry">
+						 {{Form::open([
+						 "class"=> "form-signin"
+						 ])}}
+				<p>
+					<label for="username">Correo:</label><br/>
+					<input type="text" class="form-control" name="username" id="username">
+				</p>
+				<p>
+					<label for="password">Contraseña:</label><br/>
+					<input type="password" class="form-control" name="password" id="password">
+				</p>
   <p><input type="submit" class="btn btn-lg btn-primary btn-block" value="Acceder"></p>
   {{Form::close()}}
- 	 </div>
- </div>
+					</div>
+				</article>
+			</div>
+			</section>
+		</div>
 </div>
+</section>
+</main>
 @stop
