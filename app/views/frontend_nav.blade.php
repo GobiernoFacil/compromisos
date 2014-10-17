08c74776169e8ed572cbbe79f865d4d54b6fcd22
hugovom <hugo.om@gmail.com>
2014-10-17 10:29:46 -0500

    corrige rutas de la navegación principal en el tablero

--- app/views/frontend_nav.blade.php
+++ app/views/frontend_nav.blade.php
@@ -4,16 +4,16 @@
 		<div class="row">
 			<div class="col-md-5">
 				<h1 id="logo">
-				    <a href="http://aga.ensambledev.com"><strong>Alianza</strong> para el <strong>Gobierno Abierto</strong></a>
+				    <a href="http://gobabiertomx.org/"><strong>Alianza</strong> para el <strong>Gobierno Abierto</strong></a>
 				</h1>
 			</div>
 			<nav class="col-md-7">
 				<ul>
-				<li><a href="http://aga.ensambledev.com/mision-y-objetivos/">Misión y Objetivos</a></li>
-				<li><a href="#plan">Antecedentes</a></li>
-				<li><a href="http://aga.ensambledev.com/blog/">Blog</a></li>
-				<li><a href="http://aga.ensambledev.com/preguntas-frecuentes/">Preguntas Frecuentes</a></li>
-				<li {{ Request::is( '/') ? 'class="current"' : '' }}><a href="http://tablero.inegifacil.com/">Tablero</a></li>
+				<li><a href="http://gobabiertomx.org/mision-y-objetivos/">Misión y Objetivos</a></li>
+				<li><a href="http://gobabiertomx.org/alianza-mexico//">Antecedentes</a></li>
+				<li><a href="http://gobabiertomx.org/blog/">Blog</a></li>
+				<li><a href="http://gobabiertomx.org/preguntas-frecuentes/">Preguntas Frecuentes</a></li>
+				<li {{ Request::is( '/') ? 'class="current"' : '' }}><a href="http://tablero.gobabiertomx.org/">Tablero</a></li>
 				</ul>
 			</nav>
 		</div>
